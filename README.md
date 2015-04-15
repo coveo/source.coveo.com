735f41a6953f53e8cd9e6c25e64a752232793dbc
Pierre-Alexandre St-Jean <pa@stjean.me>
2015-04-15 15:56:02 -0400

    Want some love, here is some NICE LOVE!
    maybe we do too much api calls tough (20 :S )

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # Open-source @ Coveo
 
+
+Uses:
+
+Nice web technologies
+
+- flexbox
+- es5 (yes we're late on es6)
+- [riotjs](https://muut.com/riotjs/)
+- [reqwest](https://github.com/ded/reqwest)
+
 ## TODO:
 
+- Add support into coveo.analytics.js
 - Get a design
+  - light canvas animated background
+  - Tiles ala http://heliom.ca/
+  - Build with <3 replace
+  - Orange instead of red
+  - No last commit info
+  - commit graph
 - Queries
   - repos
   - stats
--- scripts/main.js
+++ scripts/main.js
@@ -1,54 +1,131 @@
-(function(){
-  "use strict";
+/*global window,riot,reqwest */
+"use strict";
+// console.log
+// _.-~-.
+// 7''  Q..\
+// _7         (_
+// _7  _/    _q.  /
+// _7 . ___  /VVvv-'_                                            .
+// 7/ / /~- \_\\      '-._     .-'                      /       //
+// ./ ( /-~-/||'=.__  '::. '-~'' {             ___   /  //     ./{
+// V   V-~-~| ||   __''_   ':::.   ''~-~.___.-'' _/  // / {_   /  {  /
+// VV/-~-~-|/ \ .'__'. '.    '::                     _ _ _        ''.
+// / /~~~~||VVV/ /  \ )  \        _ __ ___   ___ ___(_) | | __ _   .::'
+// / (~-~-~\\.-' /    \'   \::::. | '_ ` _ \ / _ \_  / | | |/ _` | :::'
+// /..\    /..\__/      '     '::: | | | | | | (_) / /| | | | (_| | ::'
+// vVVv    vVVv                 ': |_| |_| |_|\___/___|_|_|_|\__,_| ''
+//
+// Hi there, nice to meet you!
+//
+// Interested in having a direct impact on hundreds of millions of users? Join
+// Mozilla, and become part of a global community that’s helping to build a
+// brighter future for the Web.
+//
+// Visit https://careers.mozilla.org to learn about our current job openings.
+// Visit https://www.mozilla.org/contribute for more ways to get involved and
+// help support Mozilla.
 
-  var githubUser = "coveo";
 
-  function get_public_repos(){
-    var h = {
-      method: "GET",
-      url: "https://api.github.com/users/Coveo/repos",
-      params: {
-        type: "public",
-        sort: "updated"
-      }
-    };
-    return [];
+// Private Storage
+var _githubUser = "coveo";
+var _stats = {
+  repositories: 0,
+  size: 0,
+  openIssues: 0,
+  languages: {},
+};
+var _repositories = [];
+
+function getRepositories(){ return _repositories; }
+function getStats(){ return _stats; }
+
+// UI Setup
+var _projectsTags;
+var _globalStatsTags;
+var _langStatsTags;
+
+function _updateTags(tags){
+  if(tags){tags.forEach(function(tag){ tag.update(); }); }
+}
+
+function updateProjects(){ _updateTags(_projectsTags); }
+function updateStats(){ _updateTags(_globalStatsTags); _updateTags(_langStatsTags); }
+
+// Global Object (for debugging or fiddling around)
+var CoveoOpenSource = {
+  githubUser: _githubUser,
+  stats: _stats,
+  getRepositories: getRepositories,
+  getStats: getStats
+};
+
+window.CoveoOpenSource = CoveoOpenSource;
+
+
+riot.compile(function() {
+  _projectsTags = riot.mount("projects");
+  _globalStatsTags = riot.mount("globalstats");
+  _langStatsTags = riot.mount("langstats");
+});
+
+
+function githubRepositories(githubUser){
+  return reqwest({
+    url: "https://api.github.com/users/" + githubUser + "/repos?type=public&sort=updated",
+    type: "json"
+  });
+}
+
+function githubRepositoryPunchCard(githubUser, repository){
+  return reqwest({
+    url: "https://api.github.com/repos/" + githubUser + "/" + repository + "/stats/punch_card",
+    type: "json"
+  });
+}
+function githubRepositoryLanguages(githubUser, repository){
+  return reqwest({
+    url: "https://api.github.com/repos/" + githubUser + "/" + repository + "/languages",
+    type: "json"
+  });
+}
+
+
+function addBytesToLanguage(language, bytes){
+  if(!_stats.languages[language]){
+    _stats.languages[language] = 0;
   }
 
-
-  var repos = get_public_repos();
-
-
+  _stats.languages[language] += bytes;
+}
 
 
-  var repos_with_additional_repos = repos.map(function(repo){
-    // TODO: query for aditionnal info in repos
+// Let's query github and update the UI :)
+githubRepositories(_githubUser)
+.then(function(repos){
 
-    // GET /repos/:owner/:repo/languages
-    // GET /repos/:owner/:repo/stats/punch_card
-    // GET /repos/:owner/:repo/stats/participation
-    // GET /repos/:owner/:repo/stats/code_frequency
-    // GET /repos/:owner/:repo/stats/commit_activity
+  // Save in our repo store
+  repos.filter(function(repo){return !repo.fork; }).forEach(function(repo){
+    _repositories.push(repo);
+    _stats.repositories++;
+    _stats.size += repo.size;
+    _stats.openIssues += repo.open_issues_count;
+
+    githubRepositoryLanguages(_githubUser, repo.name).then(function(languages){
+      repo.languages = languages;
+
+      Object.keys(languages).forEach(function(language){
+        addBytesToLanguage(language, languages[language]);
+      });
+      updateStats();
+      updateProjects(); // update ui
+    });
   });
+  // Query
 
-  var projects = {
-    "coveo.analytics.js": {
-      "home": "https://github.com/Coveo/analytics.js",
-      "versions": {
-        "latest": {
-          "coveo.analytics.js": "https://s3.amazonaws.com/static.coveodemo.com/coveo.analytics.js/latest/coveo.analytics.js",
-          "coveo.analytics.min.js": "https://s3.amazonaws.com/static.coveodemo.com/coveo.analytics.js/latest/coveo.analytics.min.js"
-        },
-        "v0.1.1": {
-          "coveo.analytics.js": "https://s3.amazonaws.com/static.coveodemo.com/coveo.analytics.js/v0.1.1/coveo.analytics.js",
-          "coveo.analytics.min.js": "https://s3.amazonaws.com/static.coveodemo.com/coveo.analytics.js/v0.1.1/coveo.analytics.min.js"
-        },
-        "v0.1.0": {
-          "coveo.analytics.js": "https://s3.amazonaws.com/static.coveodemo.com/coveo.analytics.js/v0.1.0/coveo.analytics.js",
-          "coveo.analytics.min.js": "https://s3.amazonaws.com/static.coveodemo.com/coveo.analytics.js/v0.1.0/coveo.analytics.min.js"
-        }
-      }
-    }
-  };
-
-})();
+  _repositories.forEach(function(repo){
+    githubRepositoryPunchCard("coveo", repo.name).then(function(punchcard){
+      repo.punchcard = punchcard;
+      updateProjects(); // update ui
+    });
+  });
+});
